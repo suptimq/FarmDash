81ea827d0bc0ce6d256046895488cc6e0e979998
Peihan Gao <pg477@cornell.edu>
2020-05-12 08:20:49 -0400

    Added Profile and Updated 404 error page

--- Frontend/src/views/PageNotFound.vue
+++ Frontend/src/views/PageNotFound.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <div class="box-sherlock"><img class="img-sherlock" src="@/assets/notfound.png" alt="404 page not found"></div>
+    <h1 class="text-center text-404">4 0 4</h1>
     <h1 class="text-center">Page Not Found</h1>
   </div>
 </template>
@@ -7,3 +9,17 @@
 <script>
 export default {};
 </script>
+
+<style scoped>
+  .text-404{
+    font-size: 7rem;
+    font-weight: bold;
+  }
+  .box-sherlock{
+    text-align: center;
+    margin-top: 20px;
+  }
+  .img-sherlock{
+    margin:auto;
+  }
+</style>
