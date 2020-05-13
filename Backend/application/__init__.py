86f4370c857fe8c8943bd75bc49b8649495a6f49
Tian <tq42@cornell.edu>
2020-05-12 21:27:30 -0400

    flask configure replica db connection

--- Backend/application/__init__.py
+++ Backend/application/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_replicated import FlaskReplicated
 
 application = Flask(__name__, template_folder='../templates')
 
 
 application.config.from_object('config')
+
 db = SQLAlchemy(application)
+
+FlaskReplicated(application)
